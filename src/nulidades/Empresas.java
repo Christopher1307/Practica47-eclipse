458c93e3512e3d38f8b212c3225688b6a9db5501
Christopher1307 <araujochristopher715@gmail.com>
2024-04-29 20:52:27 +0100

    inicios de la practica 47

---
+++ src/nulidades/Empresas.java
@@ -0,0 +1,76 @@
+package nulidades;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Empresas {
+	private int id;
+	private String nombre;
+	private String empresa;
+	private String cartaAsociada;
+	private  List<Proveedores> proveedores;
+	
+public Empresas (int id , String nombre, String empresa, String cartaAsociada, List<Proveedores> proveedores ) {
+	this.id = id;
+	this.nombre = nombre;
+	this.empresa = empresa;
+	this.cartaAsociada = cartaAsociada;
+	this.proveedores = proveedores;
+	
+}
+
+//getters y setters
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getNombre() {
+	return nombre;
+	
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public String getEmpresa() {
+	return empresa;
+}
+
+public void setEmpresa(String empresa) {
+	this.empresa = empresa;
+}
+
+public String getCartaAsociada() {
+	return cartaAsociada;
+}
+
+public void setCartaAsociada(String cartaAsociada) {
+	this.cartaAsociada = cartaAsociada;
+}
+
+public List<Proveedores> getProveedores(){
+	return proveedores;
+}
+
+public void setProveedores(List<Proveedores> proveedores) {
+    this.proveedores = proveedores;
+}
+
+
+public void agregarProveedor(Proveedores proveedor) {
+    this.proveedores.add(proveedor);
+	}
+
+}
+
---
+++ src/nulidades/Inicio.java
@@ -0,0 +1,10 @@
+package nulidades;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
